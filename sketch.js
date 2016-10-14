9fb078851359fc889f6e735b58ca529f0c3c0d76
Pepe <63211@bwls.net>
2016-10-14 08:46:03 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,15 +7,11 @@ function draw() {
   fill( 80 , 66 , 244 , 30 );
   stroke( 80 ,66 , 244 );
   
-function setup() {
-  createCanvas( windowWidth , windowHeight ); 
-}  
- 
 }
 
 function draw() {
   ellipse( mouseX , mouseY , 100 , 100 );
   fill( 247 , 243 , 7 , 30 );
-  sroke( 247 , 243 , 7 );
+  stroke( 247 , 243 , 7 );
 
 }  
