44447f32c22ba914b7b7526bb1513f4ad1910c42
Pepe <63211@bwls.net>
2016-10-14 08:40:28 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ function draw() {
 }
 
 function draw() {
-  ellipse( mouseX , mouse Y , 100 , 100 );
+  ellipse( mouseX , mouseY , 100 , 100 );
   fill( 247 , 243 , 7 , 30 );
   sroke( 247 , 243 , 7 );
 
