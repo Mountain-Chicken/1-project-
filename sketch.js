661726e8fe034b8056ee4f019027827470186031
Pepe <63211@bwls.net>
2016-10-14 14:03:45 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,12 +3,10 @@ function setup() {
 }
 
 function draw() {
-  rect( mouseX , mouseY , 300 , 100 )
+  rect( mouseX , mouseY , 300 , 100 );
   fill( 255 , 0 , 0 );
   stroke( 0 , 255 , 0 );
-  ellipse( mouseX , mouseY , 50 , 50 );
-  fill( 175 , 15 , 170 );
-  stroke( 175 , 15 , 170 );
+  
 
 } 
 
