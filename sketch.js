08c47becc7e2a008aee8fd2ba39e270ad843db0f
Pepe <63211@bwls.net>
2016-10-14 14:08:34 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,22 @@ function draw() {
   rect( mouseX , mouseY , 300 , 100 );
   fill( 255 , 0 , 0 );
   stroke( 0 , 255 , 0 );
+    ellipse( mouseX , mouseY , 300 , 300 );
+  fill( 255 , 0 , 0 );
+  stroke( 255 , 0 , 0 );
   ellipse( mouseX , mouseY , 250 , 250 );
+  fill( 255 , 165 , 0 );
+  stroke( 255 , 165 , 0 );
+  ellipse( mouseX , mouseY , 200 , 200 );
+  fill( 245 , 255 , 0 );
+  stroke( 245 , 255 , 0 );
+  ellipse( mouseX , mouseY , 150 , 150 );
+  fill( 65 , 255 , 0 );
+  stroke( 65 , 255 , 0 );
+   ellipse( mouseX , mouseY , 100 , 100 );
+  fill( 0 , 55 , 255 );
+  stroke( 0 ,55 , 255 );
+  ellipse( mouseX , mouseY , 50 , 50 );
   fill( 175 , 15 , 170 );
   stroke( 175 , 15 , 170 );
 
