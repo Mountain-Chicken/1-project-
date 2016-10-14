a6875715f43781c882226722364de7c7b4026630
Pepe <63211@bwls.net>
2016-10-14 08:32:29 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,6 @@ function setup() {
 
 function draw() {
   ellipse( mouseX , mouseY , 200 , 200 );
-  elipse( mouseX , mouse Y , 100 , 100 );
   fill( 80 , 66 , 244 , 30 );
   stroke( 80 ,66 , 244 );
   triangle( 50 , 50 , 50 , 50 , 50 , 50 );
