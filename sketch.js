8604b03e5f163d3c037d6a5283ec982c53be6c2b
Pepe <63211@bwls.net>
2016-10-14 14:16:03 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,29 +1,9 @@
 function setup() {
   createCanvas( windowWidth , windowHeight ); 
+  elipse( 255 , 0 , 0 )
 }
 
 function draw() {
-  rect( mouseX , mouseY , 300 , 100 );
-  fill( 0 , 0 , 0 );
-  stroke( 0 , 0 , 0 );
-    ellipse( mouseX , mouseY , 300 , 300 );
-  fill( 255 , 0 , 0 );
-  stroke( 255 , 0 , 0 );
-  ellipse( mouseX , mouseY , 250 , 250 );
-  fill( 255 , 165 , 0 );
-  stroke( 255 , 165 , 0 );
-  ellipse( mouseX , mouseY , 200 , 200 );
-  fill( 245 , 255 , 0 );
-  stroke( 245 , 255 , 0 );
-  ellipse( mouseX , mouseY , 150 , 150 );
-  fill( 65 , 255 , 0 );
-  stroke( 65 , 255 , 0 );
-   ellipse( mouseX , mouseY , 100 , 100 );
-  fill( 0 , 55 , 255 );
-  stroke( 0 ,55 , 255 );
-  ellipse( mouseX , mouseY , 50 , 50 );
-  fill( 175 , 15 , 170 );
-  stroke( 175 , 15 , 170 );
 
 } 
 
